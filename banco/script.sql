b03c855377f84ed74643a49eb65540c7f0a9c0ae
Felipe357 <felipeserra.100@gmail.com>
2022-11-25 11:29:20 -0300

    Att

--- banco/script.sql
+++ banco/script.sql
@@ -16,25 +16,39 @@ create table post(
 	id integer primary key auto_increment,
 	titulo varchar(100) not null,
 	duvida varchar(500) not null,
-	tag varchar(50) not null,
     usuario varchar(20) not null,
-    foreign key (tag) references tags(tag),
     foreign key (usuario) references users(usuario)
 );
 
+create table postTags(
+    id integer primary key auto_increment,
+    tag varchar(50) not null,
+    idPost integer not null,
+    foreign key (tag) references tags(tag),
+    foreign key (idPost) references post(id)
+);
+
 create table comment(
-    idComment integer primary key not null
-	id integer,
+    idComment integer primary key not null,
+	idPost integer not null,
 	resposta varchar(500) not null,
     usuario varchar(20) not null,
     foreign key (usuario) references users(usuario),
-    foreign key (id) references post(id)
+    foreign key (idPost) references post(id)
 );
 
 create table answerComment(
-    id integer,
+    idAnswer integer primary key not null,
+    idComment integer not null,
 	resposta varchar(500) not null,
     usuario varchar(20) not null,
     foreign key (usuario) references users(usuario),
-    foreign key (id) references comment(idComment)
+    foreign key (idComment) references comment(idComment)
 );
+
+create view vw_Posts as
+select u.usuario as usuario, p.titulo as tituloPost, t.tag as tag, c.resposta as resposta, c.usuario as usuarioComment, a.resposta as respostaAnswer, a.usuario as usuarioAnswer from users u 
+inner join post p on u.usuario = p.usuario
+inner join postTags t on p.id = t.idPost
+inner join comment c on p.id = c.idPost
+inner join answerComment a on c.idComment = a.idComment;
